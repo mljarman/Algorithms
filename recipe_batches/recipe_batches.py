df0f0c3b50add66d5d06f0f0bddff4a6cca26d3c
mljarman <maggiejarman2@gmail.com>
2020-04-16 12:53:15 -0600

    stock prices and attempt recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    # look through keys in recipe
+    for rec_key, rec_value in recipe.items():
+        print(f'recipe {rec_key,rec_value}')
+    for ing_key, ing_value in ingredients.items():
+        print(f'ing {ing_key,ing_value}')
+        if ing_key != rec_key:
+            return 0
+    # search 1 by 1 by key in recipe to see if key is in ingredients
+        # if there's a key in recipe thats not in ingredients, stop
+    # if there's a key that matches, check to see if recipe value is higher
+        # if its lower, stop, no batches can be made
+        # if it's higher, divide the ingredients value by recipe value
 
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5, 'sugar': 20 }
+ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
 
+recipe_batches(recipe, ingredients)
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
 import argparse
-
+prices = [1050, 3800, 270, 1540, 2]
 def find_max_profit(prices):
-  pass
+    # set current min to first value, can only sell after buy:
+    current_min = 0
+    # set current max profit to difference between first and second values
+    current_max_profit = prices[1] - prices[0]
+    # loop through list, buy price can't be last in the list:
+    for i in range(len(prices)-1):
+        # store current minimum price:
+        if prices[i] < prices[current_min]:
+            current_min = i
+            print(f'min: {prices[current_min]}')
+            # loop through numbers after current_min and calculate profit:
+            for x in range(current_min + 1, len(prices)):
+                profit = prices[x] - prices[current_min]
+                # store largest max_profit:
+                if profit > current_max_profit:
+                    current_max_profit = profit
+                    print(f'cur_profit: {current_max_profit}')
+    return current_max_profit
 
-
+find_max_profit(prices)
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
